3b5d110dc55910c333a12ff8a1c56d4d04aa867b
PhilHandy <handyphillip270@gmail.com>
2024-09-29 13:55:23 +0100

    added project

---
+++ scenes/player/player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const GRAVITY = 600
+const SPEED = 300
+const JUMP_FORCE = 400
+const SLOW_DOWN_TIME = 0.3
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	move_and_slide()
+
+func _process(delta):
+	var new_velocity = 0.0
+	if Input.is_action_pressed("Left"):
+		new_velocity -= SPEED
+	if Input.is_action_pressed("Right"):
+		new_velocity += SPEED
+	if new_velocity == 0.0 and velocity.x != 0.0:
+		# should be slowing down
+		var slow_down = delta * (SPEED / SLOW_DOWN_TIME)
+		if velocity.x > 0.0:
+			new_velocity = max(velocity.x - slow_down, 0.0)
+		else:
+			new_velocity = min(velocity.x + slow_down, 0.0)
+	if Input.is_action_pressed("Jump"):
+		if is_on_floor():
+			velocity.y -= JUMP_FORCE
+	velocity.x = new_velocity
